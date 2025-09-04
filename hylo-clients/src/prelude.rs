51292f4ce3e8b57d8acd7cd3723cfc7b4cee746e
0xPlish <plish@hylo.so>
2025-09-04 15:52:58 -0700

    Add transaction module and prelude

---
+++ hylo-clients/src/prelude.rs
@@ -0,0 +1,21 @@
+// Common external dependencies
+pub use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
+pub use anchor_client::solana_sdk::signature::Signature;
+pub use anchor_client::Cluster;
+pub use anchor_lang::prelude::Pubkey;
+pub use anyhow::Result;
+pub use fix::prelude::*;
+
+// Token types
+pub use hylo_idl::tokens::{HYUSD, JITOSOL, SHYUSD, XSOL};
+
+// Core traits and argument types
+pub use crate::transaction::{
+  BuildTransactionData, MintArgs, QuoteInput, RedeemArgs, SimulatePrice,
+  SimulatePriceWithEnv, StabilityPoolArgs, SwapArgs, TransactionSyntax,
+};
+
+// Client implementations
+pub use crate::exchange_client::ExchangeClient;
+pub use crate::program_client::{ProgramClient, VersionedTransactionData};
+pub use crate::stability_pool_client::StabilityPoolClient;
