d5cba863e89fa71f7d1241a13c3a973bb7423532
0xPlish <plish@hylo.so>
2026-02-04 15:18:59 -0800

    Drop try_from for tokens, and paste macro

--- hylo-idl/src/tokens.rs
+++ hylo-idl/src/tokens.rs
@@ -1,24 +1,6 @@
 use anchor_lang::prelude::{pubkey, Pubkey};
-use anyhow::{anyhow, Result};
 use fix::prelude::{N6, N9};
 use fix::typenum::Integer;
-use paste::paste;
-
-macro_rules! try_from_pubkey {
-  ($token:ty) => {
-    paste! {
-       impl TryFrom<Pubkey> for $token {
-         type Error = anyhow::Error;
-         fn try_from(k: Pubkey) -> Result<Self> {
-           match k {
-             $token::MINT => Ok($token),
-             _ => Err(anyhow!("Hylo: {k} is not a supported token.")),
-           }
-         }
-       }
-    }
-  };
-}
 
 pub trait TokenMint {
   type Exp: Integer;
@@ -32,16 +14,12 @@ impl TokenMint for HYUSD {
   const MINT: Pubkey = pubkey!("5YMkXAYccHSGnHn9nob9xEvv6Pvka9DZWH7nTbotTu9E");
 }
 
-try_from_pubkey!(HYUSD);
-
 pub struct SHYUSD;
 
 impl TokenMint for SHYUSD {
   type Exp = N6;
   const MINT: Pubkey = pubkey!("HnnGv3HrSqjRpgdFmx7vQGjntNEoex1SU4e9Lxcxuihz");
 }
-
-try_from_pubkey!(SHYUSD);
 
 pub struct XSOL;
 
@@ -50,8 +28,6 @@ impl TokenMint for XSOL {
   const MINT: Pubkey = pubkey!("4sWNB8zGWHkh6UnmwiEtzNxL4XrN7uK9tosbESbJFfVs");
 }
 
-try_from_pubkey!(XSOL);
-
 pub struct JITOSOL;
 
 impl TokenMint for JITOSOL {
@@ -59,13 +35,9 @@ impl TokenMint for JITOSOL {
   const MINT: Pubkey = pubkey!("J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn");
 }
 
-try_from_pubkey!(JITOSOL);
-
 pub struct HYLOSOL;
 
 impl TokenMint for HYLOSOL {
   type Exp = N9;
   const MINT: Pubkey = pubkey!("hy1oXYgrBW6PVcJ4s6s2FKavRdwgWTXdfE69AxT7kPT");
 }
-
-try_from_pubkey!(HYLOSOL);
