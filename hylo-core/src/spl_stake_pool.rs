150efa1df607da5675006b05f9914e3ebf82f04e
0xPlish <plish@hylo.so>
2026-03-19 12:52:24 -0700

    Add size information and docstrings to SplStakePool

--- hylo-core/src/spl_stake_pool.rs
+++ hylo-core/src/spl_stake_pool.rs
@@ -1,8 +1,10 @@
 use anchor_lang::prelude::{ProgramError, Result};
-use fix::prelude::{UFix64, N9};
+use fix::prelude::*;
+use std::mem::size_of;
 
-pub const TOTAL_LAMPORTS_OFFSET: usize = 258;
-pub const POOL_TOKEN_SUPPLY_OFFSET: usize = TOTAL_LAMPORTS_OFFSET + 8;
+const U64_SIZE: usize = size_of::<u64>();
+const TOTAL_LAMPORTS_OFFSET: usize = 258;
+const POOL_TOKEN_SUPPLY_OFFSET: usize = TOTAL_LAMPORTS_OFFSET + U64_SIZE;
 
 /// Simplified view of stake pool PDA used in all SPL LST programs.
 #[derive(Debug)]
@@ -12,16 +14,17 @@ pub struct SplStakePool {
 }
 
 impl SplStakePool {
+  /// Deserializes [`SplStakePool`] from Borsh account data.
   pub fn from_bytes(data: &[u8]) -> Result<SplStakePool> {
     let total_lamports = data
-      .get(TOTAL_LAMPORTS_OFFSET..TOTAL_LAMPORTS_OFFSET + 8)
+      .get(TOTAL_LAMPORTS_OFFSET..TOTAL_LAMPORTS_OFFSET + U64_SIZE)
       .and_then(|b| b.try_into().ok())
       .map(u64::from_le_bytes)
       .map(UFix64::new)
       .ok_or(ProgramError::InvalidAccountData)?;
 
     let pool_token_supply = data
-      .get(POOL_TOKEN_SUPPLY_OFFSET..POOL_TOKEN_SUPPLY_OFFSET + 8)
+      .get(POOL_TOKEN_SUPPLY_OFFSET..POOL_TOKEN_SUPPLY_OFFSET + U64_SIZE)
       .and_then(|b| b.try_into().ok())
       .map(u64::from_le_bytes)
       .map(UFix64::new)
@@ -31,5 +34,13 @@ impl SplStakePool {
       total_lamports,
       pool_token_supply,
     })
+  }
+
+  /// Computes the true price of the LST via `total_lamports / pool_token_supply`.
+  #[must_use]
+  pub fn true_price(&self) -> Option<UFix64<N9>> {
+    self
+      .total_lamports
+      .mul_div_floor(UFix64::one(), self.pool_token_supply)
   }
 }
