f1e9924a62e1c5f12ca6177dd32030242fbd2374
0xPlish <plish@hylo.so>
2026-04-08 19:36:38 -0500

    Fix SDK version check script

--- bin/check-sdk-version.sh
+++ bin/check-sdk-version.sh
@@ -13,14 +13,15 @@ function check-sdk-version() {
   # Calculate expected version
   EXPECTED_VERSION_PATCHED="${MAIN[0]}.${MAIN[1]}.$((MAIN[2] + 1))"
   EXPECTED_VERSION_MINOR="${MAIN[0]}.$((MAIN[1] + 1)).0"
+  EXPECTED_VERSION_MAJOR="$((MAIN[0] + 1)).0.0"
 
-  # Check if versions have the same major and minor, and patch is exactly one higher
-  if [ "$CURRENT_VERSION" = "$EXPECTED_VERSION_PATCHED" ] || [ "$CURRENT_VERSION" = "$EXPECTED_VERSION_MINOR" ];
+  # Check if version is exactly one patch, minor, or major bump
+  if [ "$CURRENT_VERSION" = "$EXPECTED_VERSION_PATCHED" ] || [ "$CURRENT_VERSION" = "$EXPECTED_VERSION_MINOR" ] || [ "$CURRENT_VERSION" = "$EXPECTED_VERSION_MAJOR" ];
   then
       echo "$3 SDK version check passed"
   else
-      echo "Error: $3 SDK version ($CURRENT_VERSION) must be either one patch or full minor version above main branch ($MAIN_VERSION)"
-      echo "Please update $3 SDK version to: $EXPECTED_VERSION_PATCHED or $EXPECTED_VERSION_MINOR"
+      echo "Error: $3 SDK version ($CURRENT_VERSION) must be one patch, minor, or major version above main branch ($MAIN_VERSION)"
+      echo "Please update $3 SDK version to: $EXPECTED_VERSION_PATCHED, $EXPECTED_VERSION_MINOR, or $EXPECTED_VERSION_MAJOR"
       exit 1
   fi
 }
